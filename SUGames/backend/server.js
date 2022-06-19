03e8c7b1b632adbcc76b59210f880e72e270ab4e
borakdeniz <eminbora.akdeniz@gmail.com>
2022-06-19 23:15:30 +0300

    proxy muhabbeti

--- SUGames/backend/server.js
+++ SUGames/backend/server.js
@@ -7,7 +7,7 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
