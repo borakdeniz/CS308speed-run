dd3c4aef70b8197bf2d4d019b05a22b291411467
borakdeniz <eminbora.akdeniz@gmail.com>
2022-06-19 20:57:15 +0300

    product page fix

--- SUGames/frontend/src/App.js
+++ SUGames/frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       </header>
       <main>
         <h1>Featured Products</h1>
-        <div classname="products">
+        <div className="products">
           {data.products.map((product) => (
             <div className="product" key={product.slug}>
               <img src={product.image} alt={product.name} />
