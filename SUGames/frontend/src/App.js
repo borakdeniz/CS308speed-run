46141a0c7ed9fdc7415cd889fea9df3328c65180
borakdeniz <eminbora.akdeniz@gmail.com>
2022-06-19 20:51:25 +0300

    product list

--- SUGames/frontend/src/App.js
+++ SUGames/frontend/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-        <a href="/">SU Games | Home</a>
+        <a href="/">SU Games</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div classname="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
