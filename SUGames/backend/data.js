83b95521cc514cf3b2bc7bccea311aeaa62cf4e6
borakdeniz <eminbora.akdeniz@gmail.com>
2022-06-19 22:47:53 +0300

    creating backend and connecting it to frontend

---
+++ SUGames/backend/data.js
@@ -0,0 +1,45 @@
+const data = {
+  products: [
+    {
+      name: 'Spiderman',
+      slug: 'spiderman-game1',
+      image: './images/spidermanimg.jpg',
+      edition: 'Game of the Year',
+      description: 'best game ever',
+      genre: [],
+      price: 45,
+      distributor: 'Sony',
+      comments: [],
+      rating: 4.5,
+      stock: '2',
+    },
+    {
+      name: 'Spiderman',
+      slug: 'spiderman-game2',
+      image: './images/spidermanimg.jpg',
+      edition: 'Game of the Year',
+      description: 'best game ever',
+      genre: [],
+      price: 45,
+      distributor: 'Sony',
+      comments: [],
+      rating: 4.5,
+      stock: '2',
+    },
+    {
+      name: 'Spiderman',
+      slug: 'spiderman-game3',
+      image: './images/spidermanimg.jpg',
+      edition: 'Game of the Year',
+      description: 'best game ever',
+      genre: [],
+      price: 45,
+      distributor: 'Sony',
+      comments: [],
+      rating: 4.5,
+      stock: '2',
+    },
+  ],
+};
+
+export default data;
--- SUGames/frontend/src/App.js
+++ SUGames/frontend/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import data from './data';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
